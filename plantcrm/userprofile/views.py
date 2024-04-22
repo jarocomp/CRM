3cf7104baae78cebd9231d8a8f3c7dc0e6c2801f
Jaroslav Petrovsky <petrovskyjaroslav@gmail.com>
2024-04-22 23:20:45 +0200

    Admin added

---
+++ plantcrm/userprofile/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+
+def signup(request):
+    form = UserCreationForm()
+
+    return render(request, 'userprofile/signup.html',{
+        'form':form
+    })
